221cb0e822fa8ab78e7c13ec7231a28c265c948a
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2024-10-18 16:35:03 +0100

    Add getHumanChoice() to get player's move
    Confirm that the player's input is a valid choice.
    Return the input value if it is.
    Display a message to the player if it isn't.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const humanScore = 0;
+const computerScore = 0;
+
+console.log(computerScore, humanScore);
 function getComputerChoice() {
     let computerRandomNumber;
     let computerChoice;
@@ -5,14 +9,26 @@ function getComputerChoice() {
     // generate a random number to select one of the three game choices
     computerRandomNumber = Math.floor(Math.random() * 10);
     if (computerRandomNumber <= 3) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (computerRandomNumber >= 4 && computerRandomNumber <= 7) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
 
     return computerChoice;
 }
 
-console.log(getComputerChoice());   // test that the function works as expected
+function getHumanChoice() {
+    let humanChoiceInput;
+    let initialHumanInput = prompt("Your move\n(Enter 'rock', 'paper', or 'scissors'): ", "rock");
+
+    if (initialHumanInput.toLowerCase() === "rock" 
+    || initialHumanInput.toLowerCase() === "paper"
+    || initialHumanInput.toLowerCase() === "scissors") {
+        humanChoiceInput = initialHumanInput;
+        return humanChoiceInput;
+    } else {
+        alert("That isn't a valid move in Rock Paper Scissors!");
+    }
+}
